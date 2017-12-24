e62c1eead3c57cd4137bd2f957a14a94cc5eca8c
Andrea Chen <andreachen.ac@gmail.com>
2017-12-23 18:07:48 -0800

    Create CardSection. Able to see image and text. Trying to get these on the same line.

--- src/components/Bin.js
+++ src/components/Bin.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text } from 'react-native';
+import { AppRegistry, StyleSheet, View, Text, Image } from 'react-native';
 import MapView from 'react-native-maps';
+import Card from './Card';
+import CardSection from './CardSection';
 
 class Bin extends Component {
 
@@ -27,15 +29,53 @@ class Bin extends Component {
 
         <MapView.Callout>
           <Card>
-            
+            <CardSection>
+              <View style={styles.thumbnailContainerStyle}>
+                <Image
+                  style={styles.imageStyle}
+                  source={require('../images/med_black_dot.png')}
+                  />
+                <Text>
+                  {'Garbage'}
+                </Text>
+              </View>
+            </CardSection>
+
+            <CardSection>
+
+            </CardSection>
           </Card>
-          <Text>Mup?</Text>
-          <Text>Mup?</Text>
         </MapView.Callout>
 
       </MapView.Marker>
     )
   }
 }
+
+const styles = {
+  // headerContentStyle: {
+  //   flexDirection: 'column',
+  //   justifyContent: 'space-around'
+  // },
+  // headerTextStyle: {
+  //   fontSize: 18
+  // },
+  // thumbnailStyle: {
+  //   height: 50,
+  //   width: 50
+  // },
+  thumbnailContainerStyle: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    marginLeft: 3,
+    marginRight: 3,
+  },
+  imageStyle: {
+    height: 10,
+    width: 10,
+    // flex: 1, //get image to stretch entire screen
+    // width: null //get image to stretch entire screen
+  }
+};
 
 export default Bin;
---
+++ src/components/CardSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start', //push items to the left
+    flexDirection: 'row', //defaults to column(block)
+    borderColor: '#ddd',
+    position: 'relative'
+  }
+};
+
+
+export default CardSection;
