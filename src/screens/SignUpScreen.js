2fd30c56d98a1f3348860bfeb33b1e7c9d11225e
Andrea Chen <andreachen.ac@gmail.com>
2018-01-01 13:08:29 -0800

    Create SignUpScreen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,16 +1,114 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  email: t.String,
+  username: t.String,
+  password: t.String,
+});
+
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'blue',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
+  }
+}
+
+const options = {
+  fields: {
+    email: {
+      error: 'Please enter your email address'
+    },
+    username: {
+      error: 'Please create a username'
+    },
+    password: {
+      error: 'Please create a password'
+    },
+  },
+  stylesheet: formStyles,
+};
 
 class SignUpScreen extends Component {
+
+  handleSubmit = () => {
+    const getFormData = this._form.getValue();
+    console.log('Form data: ', getFormData);
+    if (getFormData) {
+      // POST to Rails API create use route
+    }
+  }
+
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Text>SignUpScreen</Text>
-        <Text>SignUpScreen</Text>
-        <Text>SignUpScreen</Text>
+      <View style={styles.container}>
+        <Form
+          ref={ c => this._form = c }
+          type={User}
+          options={options}
+        />
+        <TouchableHighlight style={styles.signInButtonStyle} onPress={this.handleSubmit} underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 100,
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  textStyle: {
+    alignSelf: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  signInButtonStyle: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  signUpContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  }
+});
 
 export default SignUpScreen;
