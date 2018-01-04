c3b01c4de6015e286ffcbaf70b784c51e53b8b36
Andrea Chen <andreachen.ac@gmail.com>
2018-01-03 17:00:51 -0800

    Add Header component, and rendered it in user ProfileScreen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import Header from '../components/Header';
+
 class ProfileScreen extends Component {
   render() {
     return (
       <View>
-        <Text>ProfileScreen</Text>
-        <Text>ProfileScreen</Text>
+        <Header headerText={'Profile'} />
       </View>
     );
   }
