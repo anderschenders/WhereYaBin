af5d98c362f9cc035499dd2da32b85ec4ad9d56c
Andrea Chen <andreachen.ac@gmail.com>
2018-05-06 13:11:30 -0700

    Add gifs

---
+++ services/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  API_KEY: 'AIzaSyDf0igB06H4FIU01ne6E9YsNcg0g0DTm7I',
+  AUTH_DOMAIN: 'whereyabin-bf15c.firebaseapp.com',
+  DATABASE_URL: 'https://whereyabin-bf15c.firebaseio.com',
+  PROJECT_ID: 'whereyabin-bf15c',
+  STORAGE_BUCKET: 'whereyabin-bf15c.appspot.com',
+  MESSAGE_ID: '118789656440'
+};
